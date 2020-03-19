f17e8efecca887034f3776ee6b44bb30a8bd6cd8
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2020-03-19 13:10:46 -0300

    Projeto front-end para verificação de erro

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<Header title="Cod3r - Base de Conhecimento" :hideToggle="false"></Header>
+		<Menu></Menu>
+		<Content></Content>
+		<Footer></Footer>
+	</div>
+</template>
+
+<script>
+import Content from "./components/template/Content"
+import Header from "./components/template/Header"
+import Footer from "./components/template/Footer"
+import Menu from "./components/template/Menu"
+export default {
+	name: "App",
+	components: {
+		Content,
+		Header,
+		Footer,
+		Menu
+	}
+}
+</script>
+
+<style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-rows: 300px 1fr;
+		grid-template-areas: 
+			"header header" 
+			"menu content" 
+			"menu footer";
+	}
+</style>
---
+++ src/components/template/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <header class="header">
+        <a href="" class="toggle" @click="toggleMenu" v-if="!hideToggle">
+            <i class="fa fa-angle-left" :class="icon"></i>
+        </a>
+        <h1 class="title">
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon() {
+            return "fa-angle-left"
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.$store.commit("toggleMenu")
+        }
+    }
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title {
+        font-size: 1.2rem;
+        color: white;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a {
+        color: white;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: white;
+        justify-self: flex-start;
+        text-decoration: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+</style>
